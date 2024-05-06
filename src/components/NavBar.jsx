c3a381e8d9ac6f32993344a40d1e11bc51e3d31e
Safia <safialiaqat211@gmail.com.com>
2024-05-06 13:09:15 +0500

    Navbar fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import Logo from '../assets/EZITECH-LOGO-white.png';
-import { FaRegHeart } from 'react-icons/fa';
+import { FiHeart } from "react-icons/fi";
 import { IoIosSearch } from 'react-icons/io';
 import EZIBLOGS from '../assets/EZIBLOGS-DG.png';
 import Whiteezipos from '../assets/white-ezipos.png';
@@ -29,24 +29,32 @@ const NavBar = () => {
   };
   return (
     <div>
-   <div class="top-bar">
-  <div class="container">
-    <div class="d-flex flex-row ">
-      <div class=" contact-info ms-4">
-        <span ><span className='top-bar-icon'><FaPhone /> </span><span className='me-2'>+92 3455555396</span></span>
-        <span><span className='top-bar-icon'><IoLocationSharp /> </span>Office #304-B Amna Plaza, Rawalpindi, Pakistan</span>
-        <span > <span className='top-bar-clock-icon'><FaRegClock /></span>Mon - Fri 8.00 - 20.00</span>
+<div class="top-bar">
+  <div class="ms-5">
+    <div class="container">
+      <div class="contact-info ">
+        <div className="row">
+          <div className="col-md-8">
+          <span ><FaPhone /> +92 3455555396</span>
+        <span className='ms-4'><IoLocationSharp />Office #304-B Amna Plaza, Rawalpindi, Pakistan</span>
+        <span className='ms-4'><FaRegClock />Mon - Fri 8.00 - 20.00</span>
+      
+          </div>
+          <div class="col-md-4 social-icons ">
+        <span className='me-4'>
+        <a href="#" ><FaFacebook /></a> 
+        <a href="#" className='ms-3'><FaBehance /> </a>
+        <a href="#" className='ms-3'><IoLogoInstagram /></a>
+        <a href="#" className='ms-3'><FaLinkedinIn /></a>
+        <a href="#" className='ms-3'><GrYoutube /></a>
+        </span>
+        <span className='ms-4 '><IoPerson /> Login</span>
+        <span class="divider ms-2">|</span>
+        <span className='ms-2'>Register</span>
       </div>
-      <div class=" social-icons ms-4">
-        <a href="#"><FaFacebook /></a> 
-        <a href="#"><FaBehance /> </a>
-        <a href="#"><IoLogoInstagram /></a>
-        <a href="#"><FaLinkedinIn /></a>
-        <a href="#"><GrYoutube /></a>
-        <span className='ms-5'><IoPerson /> Login</span>
-        <span class="divider">|</span>
-        <span>Register</span>
+        </div>
       </div>
+      
     </div>
   </div>
 </div>
@@ -78,7 +86,7 @@ const NavBar = () => {
               <Nav>
                 <span className="navbar-text">
                   <span className="heart-icon">
-                    <FaRegHeart />
+                  <FiHeart />
                   </span>
                   <span className="search-icon">
                     <IoIosSearch />
